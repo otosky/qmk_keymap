fdd05a77db5b5d62556281c0486720c9f994f665
Oliver Tosky <olivertosky@gmail.com>
2025-09-08 20:25:34 -0700

    fix grave placement on sofle

--- sofle/keymap.c
+++ sofle/keymap.c
@@ -44,8 +44,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [NUM] = LAYOUT(
   KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                        KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-  KC_NO,   KC_LBRC, KC_7,     KC_8,     KC_9,     KC_RBRC,                         KC_GRV,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-  KC_NO,   KC_SCLN, KC_4,     KC_5,     KC_6,     KC_EQL,                          KC_LSFT,   KC_LCTL, KC_LALT, KC_LGUI, KC_NO,   KC_NO,
+  KC_NO,   KC_LBRC, KC_7,     KC_8,     KC_9,     KC_RBRC,                       KC_NO,  KC_GRV,    KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  KC_NO,   KC_SCLN, KC_4,     KC_5,     KC_6,     KC_EQL,                       KC_NO, KC_LSFT,  KC_LCTL, KC_LALT, KC_LGUI, KC_NO,
   KC_NO,   KC_GRV,  KC_1,     KC_2,     KC_3,     KC_BSLS,   MO(FUN),       KC_NO, KC_NO,     KC_COMM, KC_DOT,  KC_ALGR, KC_NO,   KC_NO,
                                 KC_NO, KC_NO, KC_NO,     KC_0,   KC_MINS,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
 ),
